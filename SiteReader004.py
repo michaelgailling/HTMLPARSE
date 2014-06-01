d5d5b36e0ac60f0ed1037f6a0d6caa2a894d949d
michaelgailling <michael.gailling@gmail.com>
2014-06-01 10:17:22 -0400

    Update SiteReader004.py

--- SiteReader004.py
+++ SiteReader004.py
@@ -6,11 +6,25 @@ homedir = os.path.abspath('') + "/"
 import requests
 import json
 """
-SITE READER V0.4
-BY: MICHAEL GAILLING
+    SITE READER V0.4
+    BY: MICHAEL GAILLING
 
-A simple and yet incomplete html parser.
-Copyright (C) 2014  Michael Gailling
+    A simple and yet incomplete html parser.
+    Copyright (C) 2014  Michael Gailling
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 VERSION 0.4 NOTES:
 -Replaced urllib with requests and json! Seriously, fuck urllib
